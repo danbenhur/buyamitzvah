d3450ff81abfe02f5330168111bf391b6009616f
Dan <danbenhur@protonmail.com>
2022-03-14 12:09:16 +0200

    add and remove from cart

--- src/components/Mitzvot/MitzvahItem/MitvahItem.js
+++ src/components/Mitzvot/MitzvahItem/MitvahItem.js
@@ -1,18 +1,30 @@
 import classes from "./MitzvahItem.module.css";
 import MitzvahItemForm from "./MitzvahItemForm";
+import { useContext } from "react";
+import CartContext from "../../../store/cart-context";
 
 const MitzvahItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
 
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   return (
-    <li className={classes.meal}>
+    <li className={classes.mitzvah}>
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MitzvahItemForm id={props.id} />
+        <MitzvahItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- src/components/Mitzvot/MitzvahItem/MitzvahItemForm.js
+++ src/components/Mitzvot/MitzvahItem/MitzvahItemForm.js
@@ -1,14 +1,33 @@
 import classes from "./MitzvahItemForm.module.css";
 import Input from "../../UI/Input";
+import { useRef, useState } from "react";
 
 const MitzvahItemForm = (props) => {
-  const onSubmitHandler = (event) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
     event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
   };
 
   return (
-    <form onSubmit={onSubmitHandler} className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -20,6 +39,7 @@ const MitzvahItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount</p>}
     </form>
   );
 };
